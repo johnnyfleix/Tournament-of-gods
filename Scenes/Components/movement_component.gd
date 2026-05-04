1c5b9ccd208a25ffaec0e7dc92e28d5adba3ff0a
Johnnyfleix <anugaolochejohn@gmail.com>
2026-05-04 11:12:18 +0100

    feat: implementing attack for the player

--- Scenes/Components/movement_component.gd
+++ Scenes/Components/movement_component.gd
@@ -5,3 +5,10 @@ extends Node
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
 	body.velocity.x = direction * speed
+	flip_body(body)
+
+func flip_body(body: CharacterBody2D) -> void:
+	if body.velocity.x > 0.0:
+		body.scale.x = body.scale.y * 1.0
+	if body.velocity.x < 0.0:
+		body.scale.x = body.scale.y * -1.0
